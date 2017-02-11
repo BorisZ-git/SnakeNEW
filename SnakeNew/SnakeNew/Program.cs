ce0f241ca0f023b1f757a23dfb814661f118de2f
BorisZ-git <ribos@bk.ru>
2017-02-11 11:37:29 +0300

    Wrote class main in program.cs +add class: snake,food,point,wall,direction without realizetion

--- SnakeNew/SnakeNew/Program.cs
+++ SnakeNew/SnakeNew/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SnakeNew
@@ -13,10 +14,10 @@ namespace SnakeNew
             Console.WriteLine("Hello. Introduce yourself please:");
             string name = Console.ReadLine();
             Console.WriteLine("Glad to see you " + name + "\nlet start");
-           // We need realize game snake.Where snake move in space that we give to her,
-           // and eat food that we will be create in random position. When snake eat food
-           // she will be grow. Game will be end if snake bite herself or hit wall of space
-           // that we give for her.
+            // We need realize game snake.Where snake move in space that we give to her,
+            // and eat food that we will be create in random position. When snake eat food
+            // she will be grow. Game will be end if snake bite herself or hit wall of space
+            // that we give for her.
 
             //Our snake will be consist of points, so the first thing that we need to do, it's realize
             //class poitns. Then we do it next step will be realize snake that consist of points, so we
@@ -27,6 +28,41 @@ namespace SnakeNew
             //in random position, after it we will realize reaction snake on hit with food. And in
             //the end we will realize function where will be instriction that reaction will be
             //if snake hit her tail or hit wall.
+            //draw the frame with class wall
+            Wall wall = new Wall(80, 25, '+');
+            wall.Draw();
+            //realize snake with help of class point
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.Right);
+            snake.Draw();
+            //create food in the window
+            Food food = new Food(80, 25, '$');
+            food = food.CreateFood();
+            food.Draw();
+            //start the game
+            while (true)
+            {
+                //check on crash
+                if (Wall.IsHit(snake)|| snake.IsHitTail())
+                {
+                    break;
+                }
+                //check on operation Eat
+                if (snake.Eat(food))
+                {
+                    food = food.CreateFood();
+                    food.Draw();
+                }
+                //pause
+                Thread.Sleep 100;
+                //check on press key
+                if (Console.KeyAvailable)
+                {
+
+                }
+                //snake move
+                snake.Move();
+            }
             Console.ReadLine();
         }
     }
